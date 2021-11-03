55f46e0305a24a082350c571e2eb451ccd2962b8
Fabiano Heizer <fabianoheizer@MacBook-Pro-de-Fabiano.local>
2021-11-02 23:00:47 -0300

    init commit

---
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -0,0 +1,51 @@
+package br.com.alura.mvc.mudi.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Pedido {
+	private String nomeProduto;
+	private BigDecimal valorProduto;
+	private LocalDate dataDaEntrega;
+	private String urlProduto;
+	private String descricao;
+	private String urlImagem;
+	
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	public BigDecimal getValorProduto() {
+		return valorProduto;
+	}
+	public void setValorProduto(BigDecimal valorProduto) {
+		this.valorProduto = valorProduto;
+	}
+	public LocalDate getDataDaEntrega() {
+		return dataDaEntrega;
+	}
+	public void setDataDaEntrega(LocalDate dataDaEntrega) {
+		this.dataDaEntrega = dataDaEntrega;
+	}
+	public String getUrlProduto() {
+		return urlProduto;
+	}
+	public void setUrlProduto(String urlProduto) {
+		this.urlProduto = urlProduto;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+	
+}
